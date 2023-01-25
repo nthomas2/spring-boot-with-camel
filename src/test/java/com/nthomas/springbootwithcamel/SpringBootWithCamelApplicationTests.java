931565f0c72f3f1aa89ea99280c8a5aa727bf307
Nathan Thomas <nate.p.thomas@gmail.com>
2023-01-25 08:11:55 -0500

    Initializr

---
+++ src/test/java/com/nthomas/springbootwithcamel/SpringBootWithCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthomas.springbootwithcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
